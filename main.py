419b8fe18457aeaa4f3da66ae3e871f2a78440a4
Eric <ethridge2004@gmail.com>
2022-05-26 14:30:59 -0400

    commit 1

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import escape
+import functions_framework
+import pandas as pd
+df = pd.read_csv('./gpi.csv')
+
+
+@functions_framework.http
+def grabber(request):
+    """
+    This function takes in a request and returns the price of the item
+    """
+    request_args = request.args
+
+    if request_args and 'SKU Description' in request_args:
+        id = request_args['SKU Description']
+    else:
+        id = "N2 Instance Ram running in Sydney"
+    return df.loc[df['SKU description'] == id, 'List price ($)']
